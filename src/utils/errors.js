ca8fbb62ca02cd6b300561a367c1ca2267150441
Christian B. Mueller <chris@cbmueller.de>
2019-07-08 12:57:48 +0200

    General: Checkmk instead of Check_MK

--- src/utils/errors.js
+++ src/utils/errors.js
@@ -7,7 +7,7 @@ const error = (message) => ({
 });
 
 export default {
-    CANCEL: error(`API request was cancelled. This has either happened because no 'Access-Control-Allow-Origin' header is present, or because of a ssl protocol error. Make sure you are running at least Check_MK version ${requiredVersion}.`),
+    CANCEL: error(`API request was cancelled. This has either happened because no 'Access-Control-Allow-Origin' header is present, or because of a ssl protocol error. Make sure you are running at least Checkmk version ${requiredVersion}.`),
     READ: error('Could not read API response, make sure the URL you provided is correct.'),
     FORMAT: error('Invalid URL format. Please make sure to include protocol and trailing slash. Example: https://checkmk.server/site/'),
     STATUS: error('Could not connect to provided URL'),
