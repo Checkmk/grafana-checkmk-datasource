a0fa42ac26806a7786fa9e41b4c5806a7524d26f
Óscar Nájera <oscar.najera@tribe29.com>
2021-09-13 15:15:32 +0200

    Start testing

---
+++ src/DataSource.test.ts
@@ -0,0 +1,11 @@
+import { buildUrlWithParams } from './DataSource';
+import { buildRequestBody } from './graphspecs';
+
+describe('URL conversions', () => {
+  it('Params', () => {
+    expect(buildUrlWithParams('hi', { A: 5, TE: 'TTI' })).toBe('hi?A=5&TE=TTI');
+  });
+  it('Request body', () => {
+    expect(buildRequestBody({ spec: ['comb', { site: 'heute' }] })).toBe('request={"spec":["comb",{"site":"heute"}]}');
+  });
+});
