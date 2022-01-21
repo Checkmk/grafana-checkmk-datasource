437d17c3f27d8a55c97d40c95fafab4ff5304ba4
Óscar Nájera <oscar.najera@tribe29.com>
2022-01-21 14:21:13 +0100

    Save nested dropdown options

--- utils/converter.py
+++ utils/converter.py
@@ -16,6 +16,21 @@ def cli_options():
     return parser
 
 
+def nested_set(target, keys, value):
+    for key in keys[:-1]:
+        target = target.setdefault(key, {})
+    target[keys[-1]] = value
+
+
+def config_set(target, keys, value):
+    nested_set(target, keys, value)
+    nested_set(
+        target,
+        ["params", "selections"] + keys,
+        {"value": value, "label": value, "isDisabled": False},
+    )
+
+
 def update_host_tags(target):
     host_tag_obj = {"group": "grp", "value": "val", "op": "op"}
     host_tags = [key for key in target if key.startswith("filter")]
@@ -25,7 +40,7 @@ def update_host_tags(target):
         obj = key[7:]
         if value := target.pop(key):
             new_key = f"host_tag_{nr}_{host_tag_obj[obj]}"
-            target["context"]["host_tags"][new_key] = value
+            config_set(target, ["context", "host_tags", new_key], value)
 
 
 def main():
@@ -33,24 +48,31 @@ def main():
     with open(args.json_file) as fd:
         dash = json.load(fd)
 
+
     for panel in dash["panels"]:
         if panel.get("datasource") == args.datasource_old:
+            panel["datasource"] = args.datasource_new
+
             for target in panel["targets"]:
                 update_host_tags(target)
 
                 if host := target.pop("host", None):
-                    target["context"]["host"] = {"host": host}
+                    config_set(target, ["context", "host", "host"], host)
                 if host := target.pop("hostregex", None):
-                    target["context"]["hostregex"] = {"host_regex": host}
+                    nested_set(target, ["context", "hostregex", "host_regex"], host)
 
                 if service := target.pop("service", None):
-                    target["context"]["service"] = {"service": service}
+                    config_set(target, ["context", "service", "service"], service)
                 if service := target.pop("serviceregex", None):
-                    target["context"]["serviceregex"] = {"service_regex": service}
+                    nested_set(
+                        target, ["context", "serviceregex", "service_regex"], service
+                    )
 
-                if service := target.pop("site", None):
-                    target["context"]["siteopt"] = {"site": service}
+                if site := target.pop("site", None):
+                    config_set(target, ["context", "siteopt", "site"], site)
 
+                if combined := target.pop("combinedgraph", None):
+                    target["params"]["graph_name"] = combined
                 if presentation := target.pop("presentiation", None):
                     target["params"]["presentation"] = presentation
 
