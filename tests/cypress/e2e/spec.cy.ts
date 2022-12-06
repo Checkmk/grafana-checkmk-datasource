eb000aa1148bb0dcc7c330f70d6090d7ac41f5f7
Matteo Stifano <matteo.stifano@tribe29.com>
2022-12-06 15:39:48 +0100

    Move function to 'helpers' module

--- tests/cypress/e2e/spec.cy.ts
+++ tests/cypress/e2e/spec.cy.ts
@@ -1,36 +1,11 @@
+import { createCmkAutomationUser } from './helpers';
+
 describe('Source configuration', () => {
   const cmkUser = 'cmkuser';
   const cmkPassword = 'somepassword123457';
 
-  function createCmkAutomationUser() {
-    cy.request({
-      method: 'DELETE',
-      url: Cypress.env('cypressToCheckmkUrl') + '/check_mk/api/1.0/objects/user_config/' + cmkUser,
-      auth: {
-        bearer: `${Cypress.env('cmkUsername')} ${Cypress.env('cmkPassword')}`,
-      },
-      failOnStatusCode: false,
-    });
-    cy.request({
-      method: 'POST',
-      url: Cypress.env('cypressToCheckmkUrl') + '/check_mk/api/1.0/domain-types/user_config/collections/all',
-      auth: {
-        bearer: `${Cypress.env('cmkUsername')} ${Cypress.env('cmkPassword')}`,
-      },
-      body: {
-        username: cmkUser,
-        fullname: cmkUser,
-        roles: ['admin'],
-        auth_option: {
-          auth_type: 'automation',
-          secret: cmkPassword,
-        },
-      },
-    });
-  }
-
   it('configures the datasource correctly', () => {
-    createCmkAutomationUser();
+    createCmkAutomationUser(cmkUser, cmkPassword);
 
     cy.visit('/');
     cy.get('input[name="user"]').type(Cypress.env('grafanaUsername'));
