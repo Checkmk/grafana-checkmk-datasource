a4b33b804328bbc7d530517424a7775bbdc295c5
Matteo Stifano <matteo.stifano@tribe29.com>
2023-01-04 11:20:32 +0100

    New test to display time-usage panel with multiple hosts
    A new e2e test is hereby introduced to display a time-usage panel by
    just selecting the desired service after multiple hosts are created.
    
    In this way, we verify the correct graphs' generation with multiple
    hosts.

--- tests/cypress/e2e/spec.cy.ts
+++ tests/cypress/e2e/spec.cy.ts
@@ -10,13 +10,19 @@ import {
 describe('e2e tests', () => {
   const cmkUser = 'cmkuser';
   const cmkPassword = 'somepassword123457';
-  const hostName = 'localhost_grafana';
+
+  const hostName0 = 'localhost_grafana0';
+  const hostName1 = 'localhost_grafana1';
 
   before(function () {
     deleteCmkAutomationUser(cmkUser, cmkPassword, false); // clean-up possible existing user
     createCmkAutomationUser(cmkUser, cmkPassword);
-    createCmkHost(hostName);
-    executeServiceDiscovery(hostName, 'new');
+
+    createCmkHost(hostName0);
+    createCmkHost(hostName1);
+
+    executeServiceDiscovery(hostName0, 'new');
+    executeServiceDiscovery(hostName1, 'new');
     activateCmkChanges('cmk');
   });
 
@@ -39,7 +45,7 @@ describe('e2e tests', () => {
     cy.contains('Data source is working').should('be.visible');
   });
 
-  it('create a new time-usage panel', { defaultCommandTimeout: 10000, retries: 2 }, () => {
+  it('time-usage panel by service (single host)', { defaultCommandTimeout: 10000, retries: 2 }, () => {
     cy.visit('/');
     cy.get('input[name="user"]').type(Cypress.env('grafanaUsername'));
     cy.get('input[name="password"]').type(Cypress.env('grafanaPassword'));
@@ -52,7 +58,7 @@ describe('e2e tests', () => {
     cy.get('input[id="react-select-7-input"]').type('Service{enter}'); // Filter -> 'Service'
 
     cy.get('input[id="input_Host"]').type('{enter}'); // Hostname -> <current host> (one entry only)
-    cy.contains(hostName).should('be.visible');
+    cy.contains(hostName0).should('be.visible');
 
     cy.get('input[id="input_Service"]').type('{enter}'); // Service -> 'Check_MK' (first entry)
     cy.contains('Check_MK').should('be.visible');
@@ -72,8 +78,40 @@ describe('e2e tests', () => {
     cy.contains('Dashboard saved').should('be.visible');
   });
 
+  it('time-usage panel by service (multiple hosts)', { defaultCommandTimeout: 10000, retries: 2 }, () => {
+    cy.visit('/');
+    cy.get('input[name="user"]').type(Cypress.env('grafanaUsername'));
+    cy.get('input[name="password"]').type(Cypress.env('grafanaPassword'));
+    cy.get('[aria-label="Login button"]').click();
+
+    cy.visit('/dashboard/new');
+    cy.get('button[aria-label="Add new panel"]').click();
+
+    cy.get('input[id="react-select-7-input"]').type('Service{enter}'); // Filter -> 'Service'
+
+    cy.get('input[id="input_Service"]').type('{enter}'); // Service -> 'Check_MK' (first entry)
+    cy.contains('Check_MK').should('be.visible');
+
+    cy.get('[class="panel-content"]').should('be.visible');
+
+    cy.get('input[id="input_Template"]').type('{enter}'); // Template -> 'Time usage by phase' (one entry)
+    cy.contains('Time usage by phase').should('be.visible');
+
+    cy.contains('CPU time in user space, ' + hostName0).should('be.visible');
+    cy.contains('CPU time in user space, ' + hostName1).should('be.visible');
+
+    const randInt = Math.floor(Math.random() * 1000);
+    cy.get('button[title="Apply changes and save dashboard"]').contains('Save').click();
+    cy.get('input[aria-label="Save dashboard title field"]').type(' ' + randInt);
+
+    cy.get('button[aria-label="Save dashboard button"]').click();
+    cy.contains('Dashboard saved').should('be.visible');
+  });
+
   after(function () {
-    deleteCmkHost(hostName);
+    deleteCmkHost(hostName0);
+    deleteCmkHost(hostName1);
+
     deleteCmkAutomationUser(cmkUser, cmkPassword);
     activateCmkChanges('cmk');
   });
