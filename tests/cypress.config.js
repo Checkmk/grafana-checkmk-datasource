89d369d035bb0fbfa61ba03205a82b845fd41372
Matteo Stifano <matteo.stifano@tribe29.com>
2023-01-11 15:40:25 +0100

    Move single tests' configuration to global configuration
    Since all tests were using the same configurations, those are moved to
    the cypress conf file.

--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -3,5 +3,7 @@ module.exports = require('cypress').defineConfig({
     baseUrl: 'http://localhost:3000',
     supportFile: false,
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    retries: 2,
+    defaultCommandTimeout: 10000,
   },
 });
