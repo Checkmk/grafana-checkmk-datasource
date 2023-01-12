87f52e2cc0d1eca9f76d83250d20a8add4bc6079
Philipp Siegmantel <philipp.siegmantel@tribe29.com>
2023-01-12 16:56:02 +0100

    remove TODO for tagged unions for autocompleters

--- src/ui/autocomplete.ts
+++ src/ui/autocomplete.ts
@@ -9,8 +9,6 @@ export type Presentation = 'lines' | 'sum' | 'average' | 'min' | 'max';
 interface AutoCompleteConfig {
   ident: string;
   value?: string;
-  // TODO: The content of "params" changes depending on "ident".
-  // TODO: Something like a tagged union would be ideal here
   params: {
     group_id?: string;
     strict?: string | boolean;
