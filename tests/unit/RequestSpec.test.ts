66def5a4d8baf12dc45c7693b3abe9b56e225337
Benedikt Seidl <benedikt.seidl@tribe29.com>
2023-03-15 16:08:27 +0100

    types derived from RequestSpec can be more generic

---
+++ tests/unit/RequestSpec.test.ts
@@ -0,0 +1,10 @@
+import { PickByValue } from '../../src/RequestSpec';
+
+type Test = PickByValue<{ foo: string; bar: number }, string>;
+const value: Test = { foo: '' };
+
+describe('we have no unit tests yet', () => {
+  it('but want just make sure PickByValue works as expected', () => {
+    expect(1).toStrictEqual(1);
+  });
+});
