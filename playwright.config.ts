68868fb1c975c18f9fdb3e615fa038bc4bafcb6b
Leonardo Petrora <29212725+lpetrora@users.noreply.github.com>
2024-09-04 16:18:51 +0200

    Migration III
    The e2e tests now run inside docker. It spawns 3 containers:
    * Grafana
    * Checkmk
    * Playwright runner
    
    In order to run the tests, first you need to manually build the plugin
    
    > npm run build
    
    With the plugin built, the tests run unattended from docker
    
    tests > docker compose up --build

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,7 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig<PluginOptions>({
   testDir: './tests/e2e/tests',
+  outputDir: './tests/results',
   fullyParallel: false,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
